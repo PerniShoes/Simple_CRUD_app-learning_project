40e34a2245c4be84efad52c60bde0a4477b1d273
PerniShoes <marek2001lenss@gmail.com>
2025-11-19 20:43:04 +0100

    Update Form1.Designer.cs

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -36,10 +36,10 @@
             // 
             // gridControl1
             // 
-            this.gridControl1.Location = new System.Drawing.Point(275, 305);
+            this.gridControl1.Location = new System.Drawing.Point(248, 297);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(805, 323);
+            this.gridControl1.Size = new System.Drawing.Size(832, 331);
             this.gridControl1.TabIndex = 1;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
